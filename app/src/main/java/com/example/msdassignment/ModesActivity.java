671f86a563e14612929ffb3c8cc2f9d6c9d5dfa6
Scorpio859 <98483288+Scorpio859@users.noreply.github.com>
2023-11-18 14:05:22 +0000

    added tabhost view for the perks and game mode activities and xmls

--- app/src/main/java/com/example/msdassignment/ModesActivity.java
+++ app/src/main/java/com/example/msdassignment/ModesActivity.java
@@ -3,6 +3,7 @@ package com.example.msdassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class ModesActivity extends AppCompatActivity {
 
@@ -10,5 +11,30 @@ public class ModesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modes);
+
+        TabHost tabHost = (TabHost)findViewById(R.id.modesTab);
+
+        tabHost.setup();
+
+        //setting up spec for 4v4 modes tab
+        TabHost.TabSpec spec = tabHost.newTabSpec("4V4 Modes");
+        spec.setContent(R.id.fourVfourModes);
+        spec.setIndicator("4V4 Modes");
+        //adding tab to tabhost instance
+        tabHost.addTab(spec);
+
+        //setting up spec for 1V1 modes tab
+        spec = tabHost.newTabSpec("1V1 Modes");
+        spec.setContent(R.id.oneVoneModes);
+        spec.setIndicator("1V1 Modes");
+        //adding defencive tab
+        tabHost.addTab(spec);
+
+        //setting up spec for 2V2 modes tab
+        spec = tabHost.newTabSpec("2V2 Modes");
+        spec.setContent(R.id.twoVtwoModes);
+        spec.setIndicator("2V2 Modes");
+        //adding assistive tab
+        tabHost.addTab(spec);
     }
 }
--- app/src/main/java/com/example/msdassignment/PerksActivity.java
+++ app/src/main/java/com/example/msdassignment/PerksActivity.java
@@ -2,7 +2,9 @@ package com.example.msdassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.DatabaseErrorHandler;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class PerksActivity extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class PerksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perks);
+
+        TabHost tabHost = (TabHost)findViewById(R.id.perksTab);
+
+        tabHost.setup();
+
+        //setting up spec for offencive perks tab
+        TabHost.TabSpec spec = tabHost.newTabSpec("Offencive");
+        spec.setContent(R.id.Offensive);
+        spec.setIndicator("Offencive");
+        //adding tab to tabhost instance
+        tabHost.addTab(spec);
+
+        //setting up spec for defencive perks tab
+        spec = tabHost.newTabSpec("Defencive");
+        spec.setContent(R.id.Defencive);
+        spec.setIndicator("Defencive");
+        //adding defencive tab
+        tabHost.addTab(spec);
+
+        //setting up spec for assistive perks tab
+        spec = tabHost.newTabSpec("Assistive");
+        spec.setContent(R.id.Assistive);
+        spec.setIndicator("Assistive");
+        //adding assistive tab
+        tabHost.addTab(spec);
     }
 }
