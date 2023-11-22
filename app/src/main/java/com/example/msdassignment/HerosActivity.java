ace93dae428509310a5d510adfa4f17e93014746
Scorpio859 <98483288+Scorpio859@users.noreply.github.com>
2023-11-22 21:44:00 +0000

    added rest of knights and vikings to hero list

--- app/src/main/java/com/example/msdassignment/HerosActivity.java
+++ app/src/main/java/com/example/msdassignment/HerosActivity.java
@@ -10,17 +10,41 @@ public class HerosActivity extends AppCompatActivity {
     private ListView listView;
     private String heroNames[] = {
             //list of the heros names
-            "Warden",
+            "Warden", //knights
             "Peacekeeper",
             "Conqueror",
-            "Lawbringer"
+            "Lawbringer",
+            "Centurion",
+            "Gladiator",
+            "Black Prior",
+            "Warmonger",
+            "Gryphon",
+            "Raider", //Vikings
+            "Warlord",
+            "Berserker",
+            "Valkyrie",
+            "Highlander",
+            "Shaman",
+            "Jormungandr"
     };
     private Integer imageid[] = {
             //images from drawable folder
-            R.drawable.wardenhead,
+            R.drawable.wardenhead,//knights pics
             R.drawable.pkhead,
             R.drawable.conqhead,
-            R.drawable.lawhead
+            R.drawable.lawhead,
+            R.drawable.centhead,
+            R.drawable.gladhead,
+            R.drawable.bphead,
+            R.drawable.warhead,
+            R.drawable.gryphhead,
+            R.drawable.raiderhead, //Vikings
+            R.drawable.warlordhead,
+            R.drawable.zerkhead,
+            R.drawable.valkhead,
+            R.drawable.hlhead,
+            R.drawable.shamanhead,
+            R.drawable.jormhead
     };
 
     @Override
